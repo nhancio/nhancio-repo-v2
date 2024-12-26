73cb545c43ecf98e0f4cf59a6e08959e339c3c62
vedantshah46 <vedantshah022@gmail.com>
2024-12-26 14:47:45 +0530

    Carrer page added + theme changed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
 import HomePage from "./MyComponent/HomePage/HomePage";
-// import Services from "./MyComponent/Services/Services";
-// import Footer from "./MyComponent/Footer/Footer";
-// import ContactUs from "./MyComponent/ContactUs/ContactUs";
+import Career from "./MyComponent/Careers/Career";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <HomePage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* Route for HomePage */}
+        <Route path="/" element={<HomePage />} />
+
+        {/* Route for Career Page */}
+        <Route path="/Career" element={<Career />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- src/MyComponent/Navbar/Navbar.jsx
+++ src/MyComponent/Navbar/Navbar.jsx
@@ -1,32 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import logo from "./Images/Nhancio.png";
 import "./Navbar.css";
-import { useState , useEffect } from "react";
-import Services from "../Services/Services";
 
 const Navbar = () => {
   const [navbarLight, setNavbarLight] = useState(false);
 
   // Change navbar background on scroll
-  const handleScroll = () => {
-    if (window.scrollY > 50) {
-      setNavbarLight(true);
-    } else {
-      setNavbarLight(false);
-    }
-  };
-
   useEffect(() => {
     const handleScroll = () => {
-      const navbar = document.querySelector(".navbar");
       if (window.scrollY > 50) {
-        navbar.classList.add("navbar-light", "bg-light");
-        navbar.classList.remove("navbar-dark", "bg-dark");
-        setNavbarLight(true); // Set navbarLight to true on scroll
+        setNavbarLight(true);
       } else {
-        navbar.classList.add("navbar-dark", "bg-dark");
-        navbar.classList.remove("navbar-light", "bg-light");
-        setNavbarLight(false); // Set navbarLight to false when at the top
+        setNavbarLight(false);
       }
     };
 
@@ -35,78 +21,77 @@ const Navbar = () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
   return (
     <>
-      <nav className={`navbar navbar-expand-lg sticky-top ${navbarLight ? 'navbar-light bg-light' : 'navbar-dark bg-dark'}`}>
-        <div className="container-fluid d-flex align-items-center">
-          {/* Logo and Brand Name */}
-          <a className="navbar-brand d-flex align-items-center me-0" href="javascript:void(0);">
-            <img
-              src={logo}
-              alt="Nhancio Logo"
-              width="40"
-              height="40"
-              className="d-inline-block align-top"
-            />
-            <span className="ms-1 gradient-text">Nhancio</span>
-          </a>
+    <nav
+      className={`navbar navbar-expand-lg sticky-top ${
+        navbarLight ? "navbar-light bg-dark" : "navbar-dark bg-light"
+      }`}
+    >
+      <div className="container-fluid d-flex align-items-center">
+        {/* Logo */}
+        <a className="navbar-brand d-flex align-items-center me-0" href="/">
+          <img
+            src={logo}
+            alt="Nhancio Logo"
+            width="40"
+            height="40"
+            className="d-inline-block align-top"
+          />
+          <span className="ms-1 gradient-text">Nhancio</span>
+        </a>
 
-          {/* Navbar toggler for mobile */}
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+        {/* Navbar toggler for mobile */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-          {/* Navbar links */}
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto">
-              {/* ms-auto keeps items aligned to the right */}
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link"  href="#services" >
-                  Services
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Our Work
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#aboutUs" >
-                  About Us
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link abc" href="#contactUsHomePage">
-                  Contact Us
-                </a>
-              </li>
-              <li className="nav-item">
-              <a
-                  className={`nav-link ${
-                    navbarLight ? "black-bg-button" : "white-bg-button"
-                  }`}
-                  href="#"
-                >
-                  Careers
-                </a>
-              </li>
-            </ul>
-          </div>
+        {/* Navbar links */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#services">
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#aboutUs">
+                About Us
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#contactUsHomePage">
+                Contact Us
+              </a>
+            </li>
+            <li className="nav-item">
+              <Link to="/Career"
+                className={`nav-link ${
+                  navbarLight ? "black-bg-button" : "white-bg-button"
+                }`}
+                
+              >
+                Careers
+              </Link>
+            </li>
+          </ul>
         </div>
-      </nav>
+      </div>
+    </nav>
     </>
   );
 };
